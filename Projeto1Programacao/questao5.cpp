bf5766791ce02a62fa2623aad9a0d3118233a72e
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2022-05-24 15:18:53 -0300

    Update questao5.cpp

--- Projeto1Programacao/questao5.cpp
+++ Projeto1Programacao/questao5.cpp
@@ -1,25 +1,32 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
 
 int soma(float num1, float num2);
 int subtracao(float num1, float num2);
 int multiplicacao(float num1, float num2);
 int divisao(float num1, float num2);
-int leValidaOperador(char op);
+//int leValidaOperador(char op);
 
 int main(){
 	
-	int continuar;
 	float num1,num2,result;
-	char op,validador;
+	char op,validador,continuar;
 	
-
+	do{
+	
+	system("cls");
+	
 	printf("---------------------\n");
 	printf("Calculadora\n");
 	printf("---------------------\n");
 	printf("N1: ");
 	scanf("%f", &num1);
 	
-	leValidaOperador(op);
+	do{
+		printf("\nOP: ");
+		scanf(" %c", &op);
+	}while(op!='+' && op!='-' && op!='/' && op!='*');
 
 	printf("\nN2: ");
 	scanf("%f", &num2);
@@ -43,19 +50,21 @@ int main(){
 			printf("%.2f / %.2f = %.2f", num1,num2,result);
 		break;
 	}
-	
-	
+	printf("\n\nDeseja fazer uma nova operacao? Digite S para sim: ");
+	scanf("%c " , &continuar);
+	fflush(stdin);
+	}while(continuar!='S' || continuar!='s');
 	
 	return 0;
 }
-int menu();
+//int menu();
 
-int leValidaOperador(char op){
-	do{
-	printf("\nOP: ");
-	scanf(" %c", &op);
-	}while(op!='+' && op!='-' && op!='/' && op!='*');
-}
+//int leValidaOperador(char op){
+//	do{
+//	printf("\nOP: ");
+//	scanf(" %c", &op);
+//	}while(op!='+' && op!='-' && op!='/' && op!='*');
+//}
 
 int soma(float num1, float num2){
    return(num1+num2);
