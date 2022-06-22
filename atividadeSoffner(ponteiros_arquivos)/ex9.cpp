342e998e6780b36bf0e3f7740759ba57822cbc73
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2022-06-21 21:58:43 -0300

    Update ex9.cpp

--- atividadeSoffner(ponteiros_arquivos)/ex9.cpp
+++ atividadeSoffner(ponteiros_arquivos)/ex9.cpp
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(){
 	
 	int inteiro = 21;
-	//char string = "Diego";
+	char string[10] = "Diego";
 	float real = 3.14;
-	
-	int *ptr_int;
-	float *ptr_float;
-	//char *ptr_int;
-
-	ptr_int = &inteiro;
-	ptr_float = &real;
-	//ptr_string = &string;
-	
+		
 	printf("Valor da variavel 'inteiro': %d\n", inteiro);
-	printf("Endereco da variavel 'inteiro': %d\n\n", ptr_int);
+	printf("Endereco da variavel 'inteiro': %p\n\n", &inteiro);
 	
-	//printf("Valor da variavel 'string': %s\n", string);
-	//printf("Endereco da variavel 'string': %s\n\n", ptr_string);
+	printf("Valor da variavel 'string': %s\n", string);
+	printf("Endereco da variavel 'string': %p\n\n", &string);
 	
 	printf("Valor da variavel 'real': %.2f\n", real);
-	printf("Endereco da variavel 'real': %f\n\n", ptr_float);
+	printf("Endereco da variavel 'real': %p\n", &real);
+	
+	return 0;
 }
-
-
-
