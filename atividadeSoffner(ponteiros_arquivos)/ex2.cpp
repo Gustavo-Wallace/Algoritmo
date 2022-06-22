b63fe9138321d79f3f506eb18f107410d958c887
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2022-06-21 22:26:46 -0300

    Update ex2.cpp

--- atividadeSoffner(ponteiros_arquivos)/ex2.cpp
+++ atividadeSoffner(ponteiros_arquivos)/ex2.cpp
@@ -1,35 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 int main(void){
 	
+	FILE *binario;
+	
 	struct dados_pessoa{
-		char nome[20];
-		int idade;
-		float salario;
+		char nome[20] = "Diego";
+		int idade = 20;
+		float salario = 10000.00;
 	};
 	
 	struct dados_pessoa pessoa;
 	
-	pessoa.nome[20] = "Gustavo Wallace";
-	pessoa.idade = 20;
-	pessoa.salario = 10.000;
-	
-	FILE *ptrArq;
-	ptrArq = fopen("binario.bin","wb");
-	
-	if(ptrArq != NULL){
-		printf("Arquivo criado com sucesso!\n");
-	}else
-		printf("\nErro:  O arquivo nao foi criado");
-	}
-	
-	fwrite(pessoa.nome,sizeof(char),20,ptrArq);
-	fwrite(pessoa.idade,sizeof(int),1,ptrArq);
-	fwrite(pessoa.salario,sizeof(float),1,ptrArq);
-	
-	fclose(ptrArq);
+	binario = fopen("binario.txt", "wb");
+	fwrite(&pessoa, 1, sizeof(pessoa), binario);
+	fclose(binario);
 	
 	return 0;
 }
